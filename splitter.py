23a97f513c4ce40ac5ef11b828f6adc32cbb6006
theoportlock <zn.tportlock@gmail.com>
2019-03-31 08:33:05 +0100

    fixed splitter

--- splitter.py
+++ splitter.py
@@ -2,7 +2,7 @@ import tester as t
 
 def a(arr):
     output = []
-    for a in range(len(arr)):
+    for a in range(1,len(arr)):
         curr = 0
         for b in range(len(arr)-a):
             if arr[b] and arr[b+a]:
@@ -11,12 +11,5 @@ def a(arr):
     return output
 
 if __name__ == "__main__":
-    arr1 = t.bitarrin()
-    print(t.bitarrout(a(arr1)))
-    '''
-    for j in range(10):
-        arr1 = a(arr1)
-        print(t.bitarrout(arr1))
-        print(t.bitarrstats(arr1))
-    '''
+    print(t.bitarrout(a(t.bitarrin())))
     
--- tis.py
+++ tis.py
@@ -1,17 +1,16 @@
 import splitter
-import basechanger
 import combinations
 import addnewelement
 import tester as t
 
 def a(arr):
-    base = [0]*14
+    base = [0]*16
     out = []
     for c in arr:
         base = addnewelement.a(base,[c])
         out.append(combinations.a(splitter.a(base)))
     return out
- 
+
 if __name__ == "__main__":
     for j in a(t.bitarrin()):
         print(t.elementno(j))
