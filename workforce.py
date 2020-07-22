9943c0763377d90c3b519fef08fe0b48fabff6a7
theoportlock <zn.tportlock@gmail.com>
2020-07-22 19:28:22 +0100

    So much clean

--- workforce.py
+++ workforce.py
@@ -11,29 +11,29 @@ import pandas as pd
 import subprocess
 
 class worker:
-    ''' A method for running bash processes in parallel according to a csv file instructions '''
-    def __init__(self, instructions):
+    ''' A method for running bash processes in parallel according to a csv file plan '''
+    def __init__(self, plan):
         # Load attributes
         self.init_time = str(time())
-        self.instruction_file = instructions
+        self.plan_file = plan
 
         # Set up logging
-        logging.basicConfig(filename=str(Path.home())+"/workforce/workforce.log", filemode="a", format="%(created).6f,"+self.init_time+",%(processName)s,%(message)s", level=logging.INFO)
-        logging.info("begin work")
+        logging.basicConfig(filename=str(Path.home())+"/workforce/run.log", filemode="a", format="%(created).6f,"+self.init_time+",%(processName)s,%(message)s", level=logging.INFO)
+        logging.info("begin init")
 
-        # Read instructions
-        logging.info("loading %s",instructions) 
-        if not self.instruction_file:
-            logging.error("no instructions file supplied")
+        # Read plan
+        logging.info("loading %s",plan) 
+        if not self.plan_file:
+            logging.error("no plan file supplied")
             quit()
-        self.instructions = pd.read_csv(instructions,names=["source","target"], na_filter=False)
-        self.instructions["weight"] = 1
-        logging.info("instructions loaded")
+        self.plan = pd.read_csv(self.plan_file, names=["source","target"], na_filter=False)
+        self.plan["weight"] = 1
+        logging.info("plan loaded")
 
-        # Graph instructions
-        logging.info("begin instruction graphing")
-        self.graph(self.instructions)
-        logging.info("instructions graphed")
+        # Graph plan
+        logging.info("begin plan graphing")
+        self.graph(self.plan)
+        logging.info("plan graphed")
         logging.info("init complete")
 
     def graph(self, df):
@@ -45,28 +45,29 @@ class worker:
         edge_alphas = [(5 + i) / (M + 4) for i in range(M)]
         plt.figure(figsize=(10, 7))
         nx.draw(G, pos=nx.spring_layout(G,k=5/math.sqrt(G.order())), with_labels=True, edge_color=edge_colors, edge_cmap=plt.cm.Blues, width=2, font_size=10)
-        plt.savefig(self.instruction_file+".pdf")
+        plt.savefig(self.plan_file+".pdf")
 
     def run(self):
-        # Run loaded instructions from the first row
+        # Run loaded plan from the first row
         logging.info("begin work")
         def task(curr):
             jobs = []
             logging.info("running %s", curr) 
             subprocess.run(curr, shell=True)
-            for i in self.instructions.loc[self.instructions["source"] == curr].index:
-                t = Process(target=task, args=[self.instructions.iloc[i]["target"]])
+            for i in self.plan.loc[self.plan["source"] == curr].index:
+                t = Process(target=task, args=[self.plan.iloc[i]["target"]])
                 jobs.append(t)
                 t.start()
             for j in jobs:
                 j.join()
-        task(self.instructions.iloc[0]["source"])
-        logging.info("work completed")
+        task(self.plan.iloc[0]["source"])
+        logging.info("work complete")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--instructions")
+    parser.add_argument("plan", nargs=argparse.REMAINDER)
     args = parser.parse_args()
 
-    current_worker = worker(args.instructions)
+    # if args.plan:
+    current_worker = worker(args.plan[0])
     current_worker.run()
