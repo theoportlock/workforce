3a7845b00ab0becccb10b10a6964d7a018a0724d
theoportlock <zn.tportlock@gmail.com>
2020-07-22 19:07:11 +0100

    Lookin tidy

--- workforce.py
+++ workforce.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
-from datetime import datetime
-from multiprocessing import Process, Queue
+from multiprocessing import Process
 from pathlib import Path
 from time import time
 import argparse
-import importlib as il
 import logging
 import math
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
-from pathlib import Path
 import subprocess
 
 class worker:
@@ -21,26 +18,26 @@ class worker:
         self.instruction_file = instructions
 
         # Set up logging
-        logging.basicConfig(filename=str(Path.home())+"/workforce/workforce.log",
-                filemode="a",
-                format="%(created).6f,"+self.init_time+",%(processName)s,%(message)s",
-                level=logging.INFO)
+        logging.basicConfig(filename=str(Path.home())+"/workforce/workforce.log", filemode="a", format="%(created).6f,"+self.init_time+",%(processName)s,%(message)s", level=logging.INFO)
+        logging.info("begin work")
 
         # Read instructions
+        logging.info("loading %s",instructions) 
         if not self.instruction_file:
             logging.error("no instructions file supplied")
             quit()
-        logging.info("loading %s",instructions) 
         self.instructions = pd.read_csv(instructions,names=["source","target"], na_filter=False)
         self.instructions["weight"] = 1
         logging.info("instructions loaded")
 
+        # Graph instructions
+        logging.info("begin instruction graphing")
         self.graph(self.instructions)
-        
+        logging.info("instructions graphed")
         logging.info("init complete")
 
     def graph(self, df):
-        # Create graph
+        # Create graph based on a dataframe
         Graphtype = nx.DiGraph()
         G = nx.from_pandas_edgelist(df, edge_attr='weight', create_using=Graphtype)
         M = G.number_of_edges()
@@ -48,11 +45,11 @@ class worker:
         edge_alphas = [(5 + i) / (M + 4) for i in range(M)]
         plt.figure(figsize=(10, 7))
         nx.draw(G, pos=nx.spring_layout(G,k=5/math.sqrt(G.order())), with_labels=True, edge_color=edge_colors, edge_cmap=plt.cm.Blues, width=2, font_size=10)
-        #plt.show()
         plt.savefig(self.instruction_file+".pdf")
 
     def run(self):
-        # Run loaded instructions
+        # Run loaded instructions from the first row
+        logging.info("begin work")
         def task(curr):
             jobs = []
             logging.info("running %s", curr) 
@@ -63,10 +60,8 @@ class worker:
                 t.start()
             for j in jobs:
                 j.join()
-        logging.info("begin excecution")
-        # start run with first row of instructions
         task(self.instructions.iloc[0]["source"])
-        logging.info("excecution completed")
+        logging.info("work completed")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
