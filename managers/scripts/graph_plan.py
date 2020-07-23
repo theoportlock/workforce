ca5ada617f637a05df6c0bf3abe2ac9f424c9a19
theoportlock <zn.tportlock@gmail.com>
2020-07-23 15:36:41 +0100

    Working on the managers

--- managers/scripts/graph_plan.py
+++ managers/scripts/graph_plan.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
 import networkx as nx
+import pandas as pd
 
-def graph(self, df):
+def graph(plan_file):
     # Create graph based on a dataframe
+
+    df = pd.read_csv(plan_file,
     Graphtype = nx.DiGraph()
     G = nx.from_pandas_edgelist(df, edge_attr='weight', create_using=Graphtype)
     M = G.number_of_edges()
     edge_colors = range(2, M + 2)
     edge_alphas = [(5 + i) / (M + 4) for i in range(M)]
     plt.figure(figsize=(10, 7))
-    nx.draw(G, pos=nx.spring_layout(G,k=5/math.sqrt(G.order())), with_labels=True, edge_color=edge_colors, edge_cmap=plt.cm.Blues, width=2, font_size=10)
+    nx.draw(G,
+            pos=nx.spring_layout(G,k=5),
+            with_labels=True,
+            edge_color=edge_colors,
+            edge_cmap=plt.cm.Blues,
+            width=2,
+            font_size=10)
     plt.savefig(self.plan_file+".pdf")
