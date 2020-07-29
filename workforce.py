45046ba928e24ae23f449a5c5c4335233bffff63
theoportlock <zn.tportlock@gmail.com>
2020-07-29 20:06:35 +0100

    tesmp

--- workforce.py
+++ workforce.py
@@ -9,17 +9,6 @@ import logging
 import os
 import subprocess
 
-class CachedProcessPoolExecutor(ProcessPoolExecutor):
-    def __init__(self):
-        super(CachedProcessPoolExecutor, self).__init__(max_workers=1)
-
-    def submit(self, fn, *args, **extra):
-        if self._work_queue.qsize() > 0:
-            print('increasing pool size from %d to %d' % (self._max_workers, self._max_workers+1))
-            self._max_workers +=1
-
-        return super(CachedProcessPoolExecutor, self).submit(fn, *args, **extra)
-
 class worker:
     ''' A method for running bash processes in parallel according to a csv file plan '''
     def __init__(self, plan_file):
@@ -42,8 +31,7 @@ class worker:
                 logging.info("running %s", curr)
                 subprocess.call(curr, shell=True)
                 for i in [k[1] for k in self.plan if k[0] == curr]:
-                    print(i)
-                    self.pool.submit(task, i)
+                    Process.pool.submit(task, i)
             logging.info("running %s", self.plan[0][0])
             subprocess.call(self.plan[0][0], shell=True)
             task(self.plan[0][1])
