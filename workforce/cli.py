fa2fcc6e61acb64f0fadfa4681a4a567c1514552
theoportlock <zn.tportlock@gmail.com>
2024-03-25 19:56:27 +1300

    Updates to cli

--- workforce/cli.py
+++ workforce/cli.py
@@ -7,11 +7,14 @@ def main():
     from .workforce import worker
     from .gui import gui
     parser = argparse.ArgumentParser()
-    parser.add_argument("plan", nargs=argparse.REMAINDER)
+    parser.add_argument("-r", "--run")
+    parser.add_argument("workflow")
     args = parser.parse_args()
-    if args.plan:
-        current_worker = worker(args.plan[0])
+    if args.run:
+        current_worker = worker(args.run)
         current_worker.run()
+    elif args.workflow:
+        gui(args.workflow)
     else:
         gui()
     return 0
