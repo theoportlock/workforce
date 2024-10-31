890a5c43f8caec6ed7925415bf12c7433689e1fa
theoportlock <zn.tportlock@gmail.com>
2024-10-31 15:31:25 +1300

    Loads from json

--- workforce/gui.py
+++ workforce/gui.py
@@ -1,9 +1,11 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 from dash import Dash, html, Input, Output, State, dcc, ctx
+import dash
 import base64
 import dash_cytoscape as cyto
 import datetime
 import io
+import json
 import numpy as np
 import pandas as pd
 import subprocess
@@ -13,13 +15,9 @@ def gui(pipeline_file=None):
     app = Dash(__name__)
     app.title = "Workforce"
     app.layout = create_layout()
-    if pipeline_file:
-        edges = pd.read_csv(pipeline_file,
-                            sep='\t',
-                            header=None).set_axis(['source','target'], axis=1)
-        elements = edges_to_elements(edges)
-        app.layout['cytoscape-elements'].elements = elements
     register_callbacks(app)
+    if pipeline_file:
+        app.layout['cytoscape-elements'].elements = load(pipeline_file)
     webbrowser.open_new('http://127.0.0.1:8050/')
     app.run_server(debug=False, use_reloader=False)
 
@@ -31,20 +29,19 @@ def create_layout():
             dcc.Download(id='download-data'),
             html.Button('Remove', id='btn-remove', n_clicks=0),
             html.Button('Run', id='btn-runproc', n_clicks=0),
-            html.Button('Swap', id='btn-swap', n_clicks=0),
+            html.Button('Connect', id='btn-connect', n_clicks=0),
         ], style={'display': 'flex', 'flex-direction': 'row', 'gap': '2px'}),
         html.Div([
-            dcc.Input(id='txt_from', value='echo "from"', type='text', style={'width': '400px', 'margin-right': '2px'}),
-            dcc.Input(id='txt_to', value='echo "to"', type='text', style={'width': '400px', 'margin-right': '2px'}),
+            dcc.Input(id='txt_node', value='echo "Input bash command"', type='text', style={'width': '400px', 'margin-right': '2px'}),
             html.Button('+', id='btn-add', n_clicks=0, style={'margin-right': '2px', 'background-color': 'lightgreen'})
         ], style={'margin-top': '2px'}),
         html.Hr(),
         cyto.Cytoscape(
             id='cytoscape-elements',
-            layout={'name': 'breadthfirst', 'directed': True},
+            layout={'name': 'preset', 'directed': True},
             style={'width': '100%', 'height': '85vh'},
             stylesheet=create_stylesheet(),
-            autoRefreshLayout=True,
+            autoRefreshLayout=False,
             responsive=True,
             elements=[]
         ),
@@ -58,17 +55,17 @@ def create_stylesheet():
             'selector': 'node',
             'style': {
                 'label': 'data(id)',
-                'font-size': '13px',
+                'font-size': '10px',
                 'width': '30px',
                 'height': '30px',
-                'text-max-width': '250px',
+                'text-max-width': '150px',
                 "text-wrap": "wrap",
-                },
+            },
         },
         {
             'selector': 'edge',
             'style': {
-                'curve-style': 'taxi',
+                'curve-style': 'bezier',
                 'target-arrow-shape': 'triangle',
                 'line-color': 'lightgray',
             }
@@ -80,24 +77,26 @@ def register_callbacks(app):
         Output('cytoscape-elements', 'elements'),
         [Input('upload-data', 'contents'),
          Input('btn-add', 'n_clicks'),
-         Input('btn-remove', 'n_clicks')],
-        [State('txt_from', 'value'),
-         State('txt_to', 'value'),
+         Input('btn-remove', 'n_clicks'),
+         Input('btn-connect', 'n_clicks')],
+        [State('txt_node', 'value'),
          State('upload-data', 'filename'),
          State('upload-data', 'last_modified'),
          State('cytoscape-elements', 'elements'),
-         State("cytoscape-elements", "selectedNodeData")],
+         State("cytoscape-elements", "selectedNodeData"),
+         State("cytoscape-elements", "selectedEdgeData")],
         prevent_initial_call=True
     )
-    def modify_network(contents, add_clicks, remove_clicks, txt_from, txt_to, filename, last_modified, elements, selected):
+    def modify_network(contents, add_clicks, remove_clicks, connect_clicks, txt_node, filename, last_modified, elements, selected_nodes, selected_edges):
         if ctx.triggered_id == 'upload-data':
             elements = handle_upload(contents)
         elif ctx.triggered_id == 'btn-add':
-            elements = add_node(elements, txt_from, txt_to)
+            elements = add_node(elements, txt_node)
         elif ctx.triggered_id == 'btn-remove':
-            elements = remove_node(elements, selected)
+            elements = remove(elements, selected_nodes, selected_edges)  # Pass selected edges to remove
+        elif ctx.triggered_id == 'btn-connect':
+            elements = connect_nodes(elements, selected_nodes)
         return elements
-
     @app.callback(
         Output('download-data', 'data'),
         [Input('btn-download', 'n_clicks')],
@@ -106,7 +105,6 @@ def register_callbacks(app):
     )
     def save_data(n_clicks, elements):
         return save_elements(elements)
-
     @app.callback(
         Output('cytoscape-elements', 'btn-runproc'),
         [Input('btn-runproc', 'n_clicks')],
@@ -115,72 +113,45 @@ def register_callbacks(app):
     )
     def run_process(n_clicks, data):
         execute_process(data)
-
-    @app.callback(
-        [Output('txt_from', 'value'),
-         Output('txt_to', 'value')],
-        [Input('cytoscape-elements', 'tapEdgeData'),
-         Input('btn-swap', 'n_clicks')],
-        [State('txt_to', 'value'),
-         State('txt_from', 'value')],
-        prevent_initial_call=True
-    )
-    def update_text_boxes(tap_edge_data, n_clicks, txt_to, txt_from):
-        if ctx.triggered_id == 'btn-swap':
-            return txt_to, txt_from
-        elif ctx.triggered_id == 'cytoscape-elements':
-            return tap_edge_data['source'], tap_edge_data['target']
+        return dash.no_update
 
 def handle_upload(contents):
-    edges = decode(contents)
-    if edges is not None:
-        return edges_to_elements(edges)
-    else:
-        return []
-
-def decode(contents):
     content_type, content_string = contents.split(',')
     decoded = base64.b64decode(content_string)
-    try:
-        edges = pd.read_csv(io.StringIO(decoded.decode('utf-8')), sep='\t', header=None).set_axis(['source','target'], axis=1)
-    except:
-        edges = None
-    return edges
+    elements = json.load(io.StringIO(decoded.decode('utf-8')))
+    return elements
 
-def add_node(elements, txt_from, txt_to):
-    edges = elements_to_edges(elements)
-    edges = pd.concat([edges, pd.DataFrame([txt_from, txt_to], index=['source', 'target']).T], axis=0, ignore_index=True).drop_duplicates()
-    return edges_to_elements(edges)
+def load(pipeline_file):
+    with open(pipeline_file, 'r') as f:
+        elements = json.load(f)
+    return elements
 
-def remove_node(elements, selected):
-    if not selected:
+def add_node(elements, txt_node):
+    elements.append({'data':{'id':txt_node,'label':txt_node}})
+    return elements
+
+def remove(elements, selected_nodes, selected_edges):
+    selected_node_ids = {node['id'] for node in selected_nodes}
+    selected_edge_pairs = {(edge['source'], edge['target']) for edge in selected_edges}
+    elements = [
+        el for el in elements
+        if el['data'].get('id') not in selected_node_ids and
+           (el['data'].get('source'), el['data'].get('target')) not in selected_edge_pairs
+    ]
+    return elements
+
+def connect_nodes(elements, selected_nodes):
+    if len(selected_nodes) < 2:
         return elements
-    else:
-        edges = elements_to_edges(elements)
-        edges = edges.loc[(edges['source'] != selected[0]['id']) & (edges['target'] != selected[0]['id'])]
-        return edges_to_elements(edges)
+    for i in range(len(selected_nodes) - 1):
+        source_node = selected_nodes[i]['id']
+        target_node = selected_nodes[i + 1]['id']
+        elements.append({'data': {'source': source_node, 'target': target_node}})
+    return elements
 
 def save_elements(elements):
-    ele = pd.concat([pd.DataFrame.from_dict(i) for i in elements], axis=1).T.set_index('id')
-    edges = ele.drop('label', axis=1).dropna().set_index('source')
-    return dcc.send_data_frame(edges.to_csv, 'mydf.tsv', sep='\t', header=False)
-
-def elements_to_edges(elements):
-    if not elements:
-        return pd.DataFrame()
-    ele = pd.concat([pd.DataFrame.from_dict(i) for i in elements], axis=1).T.set_index('id')
-    edges = ele.drop('label', axis=1).dropna().reset_index(drop=True)
-    return edges
-
-def edges_to_elements(edges):
-    nodes = np.concatenate([edges['source'].unique(), edges['target'].unique()])
-    nodes = [{'data': {'id': name, 'label': name}} for name in nodes]
-    edges = [
-        {'data': {'source': source, 'target': target}}
-        for source, target in edges[['source','target']].values
-    ]
-    elements = edges + nodes
-    return elements
+    json_data = json.dumps(elements)
+    return dcc.send_string(json_data, 'network_data.json')
 
 def execute_process(data):
     for process in data:
